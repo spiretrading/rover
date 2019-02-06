d5c90356d1e984c302076537bc9431f444c8996f
andrei-sa <andrei@spiretrading.com>
2019-02-06 17:01:01 -0500

    Document Noncopyable

--- Include/Rover/Noncopyable.hpp
+++ Include/Rover/Noncopyable.hpp
@@ -3,11 +3,34 @@
 
 namespace Rover {
 
-  struct Noncopyable {
+  /*! \class Noncopyable
+      \brief Represents a non-copyable object that can be moved.
+      \detail User classes should inherit from Noncopyable to disable copy
+              operations. Private inheritance is enough.
+  */
+  class Noncopyable {
+  protected:
+
+    //! Empty default constructor.
     Noncopyable() = default;
+
+    //! Empty default destructor.
+    /*
+      \detail Explicit definition is unnecessary but kept for rule of 5 
+              consistency
+    */
+    ~Noncopyable() = default;
+
+    //! Deleted copy constructor
     Noncopyable(const Noncopyable&) = delete;
+
+    //! Deleted copy assignment operator
     Noncopyable& operator=(const Noncopyable&) = delete;
+
+    //! Empty default move constructor
     Noncopyable(Noncopyable&&) = default;
+
+    //! Empty default move assignment operator
     Noncopyable& operator=(Noncopyable&&) = default;
   };
 }
