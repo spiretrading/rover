41c275cf8940f5c52c31a4ca4ceac83dde26c6a0
andrei-sa <andrei@spiretrading.com>
2019-02-08 12:43:56 -0500

    Use re-generate approach to handle open intervals in Range

--- Include/Rover/Range.hpp
+++ Include/Rover/Range.hpp
@@ -9,6 +9,16 @@
 
 namespace Rover {
 
+  namespace Details {
+
+    template<typename Begin, typename End>
+    void check_range(const Begin& begin, const End& end) {
+      if(end < begin) {
+        throw std::out_of_range("Invalid range!");
+      }
+    }
+  }
+
   /** Type trait used to determine what default distribution to use for a given
       type. */
   template<typename T, typename = void>
@@ -59,12 +69,16 @@ namespace Rover {
   //! Generates an argument within a range of values determined by
   //! sub-generators.
   /*!
-    \tparam B The type of reference representing the beginning of the range.
-    \tparam E The type of reference representing the end of the range.
-    \tparam D The type of distribution to produce.
-    \tparam E The type of random engine to use.
+    \tparam Begin The type of reference representing the beginning of the range.
+    \tparam End The type of reference representing the end of the range.
+    \tparam Distribution The type of distribution to produce.
+    \tparam Engine The type of random engine to use.
   */
-  template<typename Begin, typename End>
+  template<typename Begin, typename End,
+    typename Distribution = default_distribution_t<
+      std::common_type_t<typename Begin::Type, typename End::Type>>,
+    typename Engine = default_engine_t<
+      std::common_type_t<typename Begin::Type, typename End::Type>>>
   class Range : Noncopyable {
     static_assert(is_reference_v<Begin>,
         "The beginning of the range is not a Reference");
@@ -73,21 +87,16 @@ namespace Rover {
 
     public:
 
-      /** The type of distribution to produce. */
-      using Distribution = default_distribution_t<
-          std::common_type_t<typename Begin::Type, typename End::Type>>;
-
-      /** The type of random engine to use. */
-      using Engine = default_engine_t<
-          std::common_type_t<typename Begin::Type, typename End::Type>>;
-
       using Type = std::common_type_t<typename Begin::Type, typename End::Type>;
 
       //! Constructs a Range over an interval defined by its sub-generators.
       /*!
-        \param begin The generating evaluating the beginning of the range.
+        \param begin The generator evaluating the beginning of the range.
         \param end The generator evaluating the end of the range.
         \param interval The type of interval.
+        \warning begin and end must define a valid non-empty interval. No check
+                 is made if an open interval is empty, which will result in an
+                 infinite loop in generate
       */
       template<typename BeginFwd, typename EndFwd>
       Range(BeginFwd&& begin, EndFwd&& end, Interval interval =
@@ -112,47 +121,62 @@ namespace Rover {
       Range<ensure_reference_t<std::decay_t<BeginFwd>>,
       ensure_reference_t<std::decay_t<EndFwd>>>;
 
-  template<typename Begin, typename End>
+  template<typename Begin, typename End, typename Distribution, typename Engine>
   template<typename BeginFwd, typename EndFwd>
-  Range<Begin, End>::Range(BeginFwd&& begin, EndFwd&& end, Interval interval)
+  Range<Begin, End, Distribution, Engine>::Range
+      (BeginFwd&& begin, EndFwd&& end, Interval interval)
       : m_engine(std::random_device()()),
         m_begin(std::forward<BeginFwd>(begin)),
         m_end(std::forward<EndFwd>(end)),
         m_interval(interval) {}
 
-  template<typename Begin, typename End>
-  Range<Begin, End>::Range(Range<Begin, End>&& other)
+  template<typename Begin, typename End, typename Distribution, typename Engine>
+  Range<Begin, End, Distribution, Engine>::Range
+      (Range<Begin, End, Distribution, Engine>&& other)
       : m_begin(std::move(other.m_begin)),
         m_end(std::move(other.m_end)),
         m_interval(std::move(other.m_interval)) {}
 
-  template<typename Begin, typename End>
+  template<typename Begin, typename End, typename Distribution, typename Engine>
   template<typename Session>
-  constexpr typename Range<Begin, End>::Type
-      Range<Begin, End>::generate(Session& session) {
-    auto begin = m_begin.generate(session);
-    auto end = m_end.generate(session);
-    if(m_interval == Interval::OPEN || m_interval ==
-        Interval::LEFT_EXCLUSIVE) {
-      begin = NextValue(begin);
+  constexpr typename Range<Begin, End, Distribution, Engine>::Type
+      Range<Begin, End, Distribution, Engine>::generate(Session& session) {
+    while(true) {
+      auto begin = m_begin.generate(session);
+      auto end = m_end.generate(session);
+      Details::check_range(begin, end);
+      auto distribution = Distribution(begin, end);
+      auto result = distribution(m_engine);
+      switch(m_interval) {
+        case Interval::CLOSED:
+          return result;
+        case Interval::LEFT_EXCLUSIVE:
+          if(result != begin) {
+            return result;
+          }
+          break;
+        case Interval::RIGHT_EXCLUSIVE:
+          if(result != end) {
+            return result;
+          }
+          break;
+        case Interval::OPEN:
+          if(result != begin && result != end) {
+            return result; 
+          }
+          break;
+      }
     }
-    if(m_interval == Interval::OPEN || m_interval ==
-        Interval::RIGHT_EXCLUSIVE) {
-      end = PrevValue(end);
-    }
-    check_range(begin, end);
-    auto distribution = Distribution(begin, end);
-    auto result = distribution(m_engine);
-    return result;
   }
 
-  template<typename Begin, typename End>
-      auto Range<Begin, End>::build_session() const {
+  template<typename Begin, typename End, typename Distribution, typename Engine>
+      auto Range<Begin, End, Distribution, Engine>::build_session() const {
     return std::tuple_cat(m_begin.build_session(), m_end.build_session());
   }
 
-  template<typename Begin, typename End>
-  struct ImplementsConcept<Range<Begin, End>, Generator> : std::true_type {};
+  template<typename Begin, typename End, typename Distribution, typename Engine>
+  struct ImplementsConcept<Range<Begin, End, Distribution, Engine>, Generator> :
+      std::true_type {};
 }
 
 #endif
