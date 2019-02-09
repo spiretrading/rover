ea13d8753696337b11ebec4bd5be662ee81459aa
andrei-sa <andrei@spiretrading.com>
2019-02-08 19:03:52 -0500

    Use deterministic distributions & engine for Range tests

--- Tests/Source/RangeTester.cpp
+++ Tests/Source/RangeTester.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <type_traits>
+#include <vector>
 #include <catch.hpp>
 #include "Rover/Constant.hpp"
 #include "Rover/Generator.hpp"
@@ -6,59 +8,139 @@
 
 using namespace Rover;
 
+class SequentialEngine {
+  public:
+
+    template<typename SeedFwd>
+    explicit SequentialEngine(SeedFwd&&)
+        : m_current(0) {}
+
+    int operator ()() {
+      auto ret = m_current;
+      ++m_current;
+      if(m_current > 100) {
+        m_current = 0;
+      }
+      return ret;
+    }
+  
+  private:
+    int m_current;
+};
+
+class NarrowingDistribution {
+  public:
+
+    NarrowingDistribution(int begin, int end)
+        : m_begin(begin),
+          m_end(end) {}
+
+    template<typename Engine>
+    int operator ()(Engine& engine) const {
+      int val = engine();
+      return val % (m_end + 1 - m_begin) + m_begin;
+    }
+
+  private:
+    int m_begin;
+    int m_end;
+};
+
+template<typename T>
+class MiddlePeakDistribution {
+  
+  public:
+
+    MiddlePeakDistribution(T begin, T end)
+        : m_begin(begin),
+          m_end(end) {}
+
+    template<typename Engine>
+    T operator ()(Engine&) const {
+      return (m_begin + m_end) / 2;
+    }
+
+  private:
+    T m_begin;
+    T m_end;
+};
+
 TEST_CASE("test_int_range", "[Range]") {
   SECTION("Closed interval.") {
-    for(int i = 0; i < 100; ++i) {
-      auto range = Range(Constant(1), Constant(10));
-      auto result = generate(range);
-      REQUIRE(result >= 1);
-      REQUIRE(result <= 10);
+    auto range = Range<Reference<Constant<int>>, Reference<Constant<int>>,
+        NarrowingDistribution, SequentialEngine>(Constant(5), Constant(10));
+    for(int i = 0; i < 3; ++i) {
+      REQUIRE(generate(range) == 5);
+      REQUIRE(generate(range) == 6);
+      REQUIRE(generate(range) == 7);
+      REQUIRE(generate(range) == 8);
+      REQUIRE(generate(range) == 9);
+      REQUIRE(generate(range) == 10);
     }
   }
 
   SECTION("Open interval.") {
-    for(int i = 0; i < 100; ++i) {
-      auto range = Range(Constant(1), Constant(10), Interval::OPEN);
-      auto result = generate(range);
-      REQUIRE(result >= 2);
-      REQUIRE(result <= 9);
+    auto range = Range<Reference<Constant<int>>, Reference<Constant<int>>,
+        NarrowingDistribution, SequentialEngine>(Constant(5), Constant(10),
+        Interval::OPEN);
+    for(int i = 0; i < 3; ++i) {
+      REQUIRE(generate(range) == 6);
+      REQUIRE(generate(range) == 7);
+      REQUIRE(generate(range) == 8);
+      REQUIRE(generate(range) == 9);
     }
   }
 
   SECTION("Exclude left.") {
-    for(int i = 0; i < 100; ++i) {
-      auto range = Range(Constant(1), Constant(10), Interval::LEFT_EXCLUSIVE);
-      auto result = generate(range);
-      REQUIRE(result >= 2);
-      REQUIRE(result <= 10);
+    auto range = Range<Reference<Constant<int>>, Reference<Constant<int>>,
+        NarrowingDistribution, SequentialEngine>(Constant(5), Constant(10),
+        Interval::LEFT_EXCLUSIVE);
+    for(int i = 0; i < 3; ++i) {
+      REQUIRE(generate(range) == 6);
+      REQUIRE(generate(range) == 7);
+      REQUIRE(generate(range) == 8);
+      REQUIRE(generate(range) == 9);
+      REQUIRE(generate(range) == 10);
     }
   }
 
   SECTION("Exclude right.") {
-    for(int i = 0; i < 100; ++i) {
-      auto range = Range(Constant(1), Constant(10), Interval::RIGHT_EXCLUSIVE);
-      auto result = generate(range);
-      REQUIRE(result >= 1);
-      REQUIRE(result <= 9);
+    auto range = Range<Reference<Constant<int>>, Reference<Constant<int>>,
+        NarrowingDistribution, SequentialEngine>(Constant(5), Constant(10),
+        Interval::RIGHT_EXCLUSIVE);
+    for(int i = 0; i < 3; ++i) {
+      REQUIRE(generate(range) == 5);
+      REQUIRE(generate(range) == 6);
+      REQUIRE(generate(range) == 7);
+      REQUIRE(generate(range) == 8);
+      REQUIRE(generate(range) == 9);
     }
   }
 }
 
 TEST_CASE("test_float_range", "[Range]") {
-  for(int i = 0; i < 100; ++i) {
-    auto range = Range(Constant(1.), Constant(10.));
-    auto result = generate(range);
-    REQUIRE(result >= 1.);
-    REQUIRE(result <= 10.);
+  {
+    auto range = Range<Reference<Constant<float>>, Reference<Constant<float>>,
+      MiddlePeakDistribution<float>>(Constant(5.f), Constant(20.f));
+    REQUIRE(generate(range) == (5.f + 20.f) / 2);
+    REQUIRE(generate(range) == (5.f + 20.f) / 2);
+  }
+  {
+    auto range = Range<Reference<Constant<float>>, Reference<Constant<float>>,
+      MiddlePeakDistribution<float>>(Constant(50.f), Constant(100.f));
+    REQUIRE(generate(range) == (50.f + 100.f) / 2);
+    REQUIRE(generate(range) == (50.f + 100.f) / 2);
+  }
+  {
+    auto range = Range<Reference<Constant<float>>, Reference<Constant<float>>,
+      MiddlePeakDistribution<float>>(Constant(5.f), Constant(10.f));
+    REQUIRE(generate(range) == (5.f + 10.f) / 2);
+    REQUIRE(generate(range) == (5.f + 10.f) / 2);
   }
 }
 
 TEST_CASE("test_mixed_types", "[Range]") {
-  for(int i = 0; i < 100; ++i) {
-    auto range = Range(Constant(1), Constant(10.));
-    auto result = generate(range);
-    static_assert(std::is_same_v<std::decay_t<decltype(result)>, double>);
-    REQUIRE(result >= 1.);
-    REQUIRE(result <= 10.);
-  }
+  auto range = Range<Reference<Constant<int>>, Reference<Constant<double>>,
+    MiddlePeakDistribution<double>>(Constant(1), Constant(9.5));
+  REQUIRE(generate(range) == (static_cast<double>(1) + 9.5) / 2);
 }
