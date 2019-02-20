28596675ce598ef54b7742b25d978d811ecddd79
Kamal <kamal@eidolonsystems.com>
2019-02-20 18:55:48 -0500

    Added Python build configuration. Updated the build config to automatically call the setup script as needed instead of manually requiring the user to invoke it.

--- setup.bat
+++ setup.bat
@@ -6,8 +6,8 @@ IF NOT EXIST Dependencies (
 )
 PUSHD Dependencies
 
-IF NOT EXIST Catch2 (
-  git clone --branch v2.2.1 https://github.com/catchorg/Catch2.git Catch2
+IF NOT EXIST Catch2-2.6.1 (
+  git clone --branch v2.6.1 https://github.com/catchorg/Catch2.git Catch2-2.6.1
 )
 
 IF NOT EXIST pybind11-2.2.4 (
