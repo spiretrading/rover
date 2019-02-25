879422adfcda9b7fecedac96f0266538c1f42835
andrei-sa <andrei@spiretrading.com>
2019-02-25 18:03:39 -0500

    Add Box

--- Include/Rover/Box.hpp
+++ Include/Rover/Box.hpp
@@ -1,7 +1,9 @@
 #ifndef ROVER_BOX_HPP
 #define ROVER_BOX_HPP
+#include <memory>
+#include <type_traits>
 #include "Rover/Evaluator.hpp"
-#include "Rover/Noncopyable.hpp"
+#include "Rover/Pointer.hpp"
 
 namespace Rover {
 
@@ -10,13 +12,91 @@ namespace Rover {
     \tparam T The type of value to generate.
   */
   template<typename T>
-  class Box : private Noncopyable {
+  class Box {
     public:
 
       //! The type of value to generate.
       using Type = T;
 
-      Type generate(Evaluator& evaluator) const;
+      //! Packs a generator or generator pointer into a Box
+      template<typename GeneratorFwd, std::enable_if_t<!std::is_same_v<
+          std::decay_t<GeneratorFwd>, Box>>* = nullptr>
+      explicit Box(GeneratorFwd&& gen);
+
+      Type generate(Evaluator& evaluator);
+
+    private:
+
+      struct WrapperBase {
+        virtual ~WrapperBase() = default;
+        virtual Type generate(Evaluator& evaluator) = 0;
+      };
+
+      template<typename Generator>
+      struct ValueWrapper;
+
+      template<typename GeneratorFwd>
+      ValueWrapper(GeneratorFwd&&) -> ValueWrapper<std::decay_t<GeneratorFwd>>;
+
+      template<typename Generator>
+      struct PointerWrapper;
+
+      template<typename GeneratorFwd>
+      PointerWrapper(GeneratorFwd&&)->PointerWrapper<std::decay_t<GeneratorFwd>>;
+
+      std::shared_ptr<WrapperBase> m_generator;
+  };
+
+  template<typename GeneratorFwd>
+  Box(GeneratorFwd&&) -> Box<GeneratorValueType<std::decay_t<GeneratorFwd>>>;
+
+  template<typename T>
+  template<typename GeneratorFwd, std::enable_if_t<!std::is_same_v<
+    std::decay_t<GeneratorFwd>, Box<T>>>*>
+  Box<T>::Box(GeneratorFwd&& gen)
+      : m_generator([&gen]() mutable {
+          if constexpr(is_object_pointer_v<std::decay_t<GeneratorFwd>>) {
+            return std::make_shared<PointerWrapper<std::decay_t<GeneratorFwd>>>(
+                std::forward<GeneratorFwd>(gen));
+          } else {
+            return std::make_shared<ValueWrapper<std::decay_t<GeneratorFwd>>>(
+                std::forward<GeneratorFwd>(gen));
+          }
+        }()) {}
+
+  template<typename T>
+  T Box<T>::generate(Evaluator& evaluator) {
+    return m_generator->generate(evaluator);
+  }
+
+  template<typename T>
+  template<typename Generator>
+  struct Box<T>::ValueWrapper : public WrapperBase {
+    template<typename GeneratorFwd, std::enable_if_t<!std::is_same_v<
+        std::decay_t<GeneratorFwd>, ValueWrapper>>* = nullptr>
+    ValueWrapper(GeneratorFwd&& gen)
+        : m_generator(std::forward<GeneratorFwd>(gen)) {}
+
+    virtual T generate(Evaluator& evaluator) override final {
+      return evaluator.evaluate(m_generator);
+    }
+
+    Generator m_generator;
+  };
+
+  template<typename T>
+  template<typename Generator>
+  struct Box<T>::PointerWrapper : public WrapperBase {
+    template<typename GeneratorFwd, std::enable_if_t<!std::is_same_v<
+        std::decay_t<GeneratorFwd>, PointerWrapper>>* = nullptr>
+    PointerWrapper(GeneratorFwd&& gen)
+        : m_generator(std::forward<GeneratorFwd>(gen)) {}
+
+    virtual T generate(Evaluator& evaluator) override final {
+      return evaluator.evaluate(*m_generator);
+    }
+
+    Generator m_generator;
   };
 }
 
---
+++ Include/Rover/Pointer.hpp
@@ -0,0 +1,38 @@
+#ifndef ROVER_POINTER_HPP
+#define ROVER_POINTER_HPP
+#include <type_traits>
+
+namespace Rover {
+
+  /** Trait to check whether a type is a pointer to a C++ object or POD
+      type. */
+  template<typename, typename = void>
+  struct is_object_pointer : std::false_type {};
+
+  template<typename T>
+  struct is_object_pointer<T, std::enable_if_t<std::is_pointer_v<T>>> :
+      std::true_type {};
+
+  template<typename T>
+  struct is_object_pointer<T, std::enable_if_t<decltype(*std::declval<T>(),
+      std::declval<T>().operator->(), std::true_type{})::value>> :
+      std::true_type {};
+
+  /** Trait to check whether a type is a pointer to a C++ object or POD
+      type. */
+  template<typename T>
+  inline constexpr bool is_object_pointer_v = is_object_pointer<T>::value;
+ 
+  /** Trait to retrieve the type of a generator even if a pointer is passed. */
+  template<typename Generator>
+  using GeneratorType = std::conditional_t<is_object_pointer_v<
+      Generator>, typename std::pointer_traits<Generator>::element_type,
+      typename Generator>;
+
+  /** Trait to retrieve the type of values produced by a generator even if a
+      pointer is passed. */
+  template<typename Generator>
+  using GeneratorValueType = typename GeneratorType<Generator>::Type;
+}
+
+#endif
