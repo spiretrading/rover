97aafb29ff63b6cd254523e4e2b10990aab59ea1
andrei-sa <andrei@spiretrading.com>
2019-03-05 11:43:22 -0500

    Remove specialization exports

--- Python/Source/Evaluator.cpp
+++ Python/Source/Evaluator.cpp
@@ -8,11 +8,5 @@ using namespace Rover;
 void Rover::export_evaluator(module& module) {
   pybind11::class_<Evaluator>(module, "Evaluator")
     .def(pybind11::init<>())
-    .def("evaluate", &Evaluator::evaluate<Box<pybind11::object>>)
-    .def("evaluate", &Evaluator::evaluate<Box<int>>)
-    .def("evaluate", &Evaluator::evaluate<Box<long>>)
-    .def("evaluate", &Evaluator::evaluate<Box<float>>)
-    .def("evaluate", &Evaluator::evaluate<Box<double>>)
-    .def("evaluate", &Evaluator::evaluate<Box<unsigned>>)
-    .def("evaluate", &Evaluator::evaluate<Box<unsigned long>>);
+    .def("evaluate", &Evaluator::evaluate<Box<pybind11::object>>);
 }
--- Python/Source/Rover.cpp
+++ Python/Source/Rover.cpp
@@ -9,17 +9,5 @@ using namespace Rover;
 PYBIND11_MODULE(rover, module) {
   export_evaluator(module);
   export_box(module);
-  export_box<int>(module, "Int");
-  export_box<long>(module, "Long");
-  export_box<float>(module, "Float");
-  export_box<double>(module, "Double");
-  export_box<unsigned>(module, "Unsigned");
-  export_box<unsigned long>(module, "UnsignedLong");
   export_constant(module);
-  export_constant<int>(module, "Int");
-  export_constant<long>(module, "Long");
-  export_constant<float>(module, "Float");
-  export_constant<double>(module, "Double");
-  export_constant<unsigned>(module, "Unsigned");
-  export_constant<unsigned long>(module, "UnsignedLong");
 }
