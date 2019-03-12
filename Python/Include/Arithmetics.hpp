483608f956fae03c9bad154bc6380740b4dd8de2
andrei-sa <andrei@spiretrading.com>
2019-03-12 18:01:22 -0400

    Shift SFINAE on return type for python object operators

--- Python/Include/Arithmetics.hpp
+++ Python/Include/Arithmetics.hpp
@@ -20,79 +20,79 @@ namespace pybind11 {
   object abs(const object& obj);
   object floor(const object& obj);
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  bool operator <(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, bool> 
+      operator <(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__lt__")(lhs,
       rhs).cast<bool>();
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  bool operator <=(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, bool>
+      operator <=(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__le__")(lhs,
       rhs).cast<bool>();
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  bool operator >(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, bool>
+      operator >(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__gt__")(lhs,
       rhs).cast<bool>();
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  bool operator >=(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, bool>
+      operator >=(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__ge__")(lhs,
       rhs).cast<bool>();
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  bool operator ==(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, bool>
+      operator ==(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__eq__")(lhs,
       rhs).cast<bool>();
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  bool operator !=(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, bool>
+      operator !=(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__ne__")(lhs,
       rhs).cast<bool>();
   }
   
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  object operator +(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, object>
+      operator +(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__add__")(lhs,
       rhs);
   }
   
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  object operator -(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, object>
+      operator -(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__sub__")(lhs,
       rhs);
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  object operator *(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, object>
+      operator *(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__mul__")(lhs,
       rhs);
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  object operator /(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, object>
+      operator /(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__floordiv__")(lhs,
       rhs);
   }
 
-  template<typename T1, typename T2, std::enable_if_t<
-    Rover::Details::contain_objects_v<T1, T2>>* = nullptr>
-  object operator %(const T1& lhs, const T2& rhs) {
+  template<typename T1, typename T2>
+  std::enable_if_t<Rover::Details::contain_objects_v<T1, T2>, object>
+      operator %(const T1& lhs, const T2& rhs) {
     return Rover::Details::import_operator().attr("__mod__")(lhs,
       rhs);
   }
