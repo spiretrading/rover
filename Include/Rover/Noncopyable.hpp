3352c2ddc8e8a66ea0d038c2854404fd99bb187c
andrei-sa <andrei@spiretrading.com>
2019-03-14 14:36:28 -0700

    Fix Noncopyable.hpp namespace formatting

--- Include/Rover/Noncopyable.hpp
+++ Include/Rover/Noncopyable.hpp
@@ -2,18 +2,18 @@
 #define ROVER_NONCOPYABLE_HPP
 
 namespace Rover {
-  namespace Details {
+namespace Details {
 
-    /** Base class that disables default copy construction and assignment. */
-    class Noncopyable {
-      protected:
-	constexpr Noncopyable() = default;
-	constexpr Noncopyable(Noncopyable&&) = default;
-	Noncopyable(const Noncopyable&) = delete;
-	Noncopyable& operator =(const Noncopyable&) = delete;
-	Noncopyable& operator =(Noncopyable&&) = default;
-    };
-  }
+  /** Base class that disables default copy construction and assignment. */
+  class Noncopyable {
+    protected:
+      constexpr Noncopyable() = default;
+      constexpr Noncopyable(Noncopyable&&) = default;
+      Noncopyable(const Noncopyable&) = delete;
+      Noncopyable& operator =(const Noncopyable&) = delete;
+      Noncopyable& operator =(Noncopyable&&) = default;
+  };
+}
 
   using Noncopyable = Details::Noncopyable;
 }
