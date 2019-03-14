dd68ceb22a96132604b0509376bac59862da09ba
andrei-sa <andrei@spiretrading.com>
2019-03-14 11:29:16 -0700

    Add dependencies to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,21 @@ if [ ! -d "Dependencies" ]; then
   mkdir Dependencies
 fi
 pushd Dependencies
-if [ ! -d "Catch2-2.2.1" ]; then
-  git clone --branch v2.2.1 https://github.com/catchorg/Catch2.git Catch2-2.2.1
-  if [ -d "Catch2-2.2.1" ]; then
-    pushd Catch2-2.2.1
+if [ ! -d "Catch2-2.6.1" ]; then
+  git clone --branch v2.6.1 https://github.com/catchorg/Catch2.git Catch2-2.6.1
+  if [ -d "Catch2-2.6.1" ]; then
+    pushd Catch2-2.6.1
+    cmake .
+    make
+    make install
+    popd
+  fi
+fi
+python3 -c "from distutils import sysconfig"
+if [ ! -d "pybind11-2.2.4" ]; then
+  git clone --branch v2.2.4 https://github.com/pybind/pybind11.git pybind11-2.2.4
+  if [ -d "pybind11-2.2.4" ]; then
+    pushd pybind11-2.2.4
     cmake .
     make
     make install
@@ -14,3 +25,4 @@ if [ ! -d "Catch2-2.2.1" ]; then
   fi
 fi
 popd
+
