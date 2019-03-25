f709d5ad6d104e2416c832426d3ce462aa5e91a6
andrei-sa <andrei@spiretrading.com>
2019-03-25 18:14:39 -0400

    Export ListTrial to Python

---
+++ Python/Include/ListTrial.hpp
@@ -0,0 +1,48 @@
+#ifndef ROVER_PYTHON_LIST_TRIAL_HPP
+#define ROVER_PYTHON_LIST_TRIAL_HPP
+#include <string_view>
+#include <pybind11/pybind11.h>
+#include "Rover/ListTrial.hpp"
+#include "Rover/Sample.hpp"
+
+namespace Rover {
+  
+  //! Exports a Trial for Python object samples
+  /*!
+    \param module The module to export the class to
+  */
+  void export_list_trial(pybind11::module& module);
+
+  //! Exports a Trial for a particular Sample type
+  /*!
+    \param module The module to export the class to
+    \param postfix The postfix to the name of the exported type
+  */
+  template<typename S>
+  void export_list_trial(pybind11::module& module, std::string_view postfix);
+
+  template<typename S>
+  void export_list_trial(pybind11::module& module, std::string_view postfix) {
+    auto name = std::string("Constant").append(postfix);
+    pybind11::class_<ListTrial<S>>(module, name.c_str())
+      .def(pybind11::init<>())
+      .def("reserve", &ListTrial<S>::reserve)
+      .def("capacity", &ListTrial<S>::capacity)
+      .def("insert", (void (ListTrial<S>::*)(const S&))
+         &ListTrial<S>::insert)
+      .def("insert", [](ListTrial<S>& out,
+           ListTrial<S> in) {
+         out.insert(std::make_move_iterator(in.begin()),
+           std::make_move_iterator(in.end()));
+       })
+      .def("__getitem__", &ListTrial<S>::operator [])
+      .def("__iter__", [](const ListTrial<S>& t) {
+         return pybind11::make_iterator(t.begin(), t.end());
+       })
+      .def("__len__", &ListTrial<S>::size);
+    pybind11::implicitly_convertible<ListTrial<S>, ListTrial<PythonSample>>();
+    pybind11::implicitly_convertible<ListTrial<PythonSample>, ListTrial<S>>();
+  }
+}
+
+#endif
---
+++ Python/Source/ListTrial.cpp
@@ -0,0 +1,25 @@
+#include "ListTrial.hpp"
+#include "Sample.hpp"
+
+using namespace pybind11;
+
+namespace Rover {
+  void export_list_trial(pybind11::module& module) {
+    class_<ListTrial<PythonSample>>(module, "ListTrial")
+      .def(init<>())
+      .def("reserve", &ListTrial<PythonSample>::reserve)
+      .def("capacity", &ListTrial<PythonSample>::capacity)
+      .def("insert", (void (ListTrial<PythonSample>::*)(const PythonSample&))
+         &ListTrial<PythonSample>::insert)
+      .def("insert", [](ListTrial<PythonSample>& out,
+           ListTrial<PythonSample> in) {
+         out.insert(std::make_move_iterator(in.begin()),
+           std::make_move_iterator(in.end()));
+       })
+      .def("__getitem__", &ListTrial<PythonSample>::operator [])
+      .def("__iter__", [](const ListTrial<PythonSample>& t) {
+         return make_iterator(t.begin(), t.end());
+       })
+      .def("__len__", &ListTrial<PythonSample>::size);
+  }
+}
