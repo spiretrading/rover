4487de93a4405ce3dbbb01204433ba63abad4d7e
andrei-sa <andrei@spiretrading.com>
2019-03-25 18:40:23 -0400

    Re-format namespaces in ListTrial.cpp

--- Python/Source/ListTrial.cpp
+++ Python/Source/ListTrial.cpp
@@ -2,24 +2,23 @@
 #include "Sample.hpp"
 
 using namespace pybind11;
+using namespace Rover;
 
-namespace Rover {
-  void export_list_trial(pybind11::module& module) {
-    class_<ListTrial<PythonSample>>(module, "ListTrial")
-      .def(init<>())
-      .def("reserve", &ListTrial<PythonSample>::reserve)
-      .def("capacity", &ListTrial<PythonSample>::capacity)
-      .def("insert", static_cast<void (ListTrial<PythonSample>::*)(const
-         PythonSample&)>(&ListTrial<PythonSample>::insert))
-      .def("insert", [](ListTrial<PythonSample>& out,
-           ListTrial<PythonSample> in) {
-         out.insert(std::make_move_iterator(in.begin()),
-           std::make_move_iterator(in.end()));
-       })
-      .def("__getitem__", &ListTrial<PythonSample>::operator [])
-      .def("__iter__", [](const ListTrial<PythonSample>& t) {
-         return make_iterator(t.begin(), t.end());
-       })
-      .def("__len__", &ListTrial<PythonSample>::size);
-  }
+void Rover::export_list_trial(pybind11::module& module) {
+  class_<ListTrial<PythonSample>>(module, "ListTrial")
+    .def(init<>())
+    .def("reserve", &ListTrial<PythonSample>::reserve)
+    .def("capacity", &ListTrial<PythonSample>::capacity)
+    .def("insert", static_cast<void (ListTrial<PythonSample>::*)(const
+       PythonSample&)>(&ListTrial<PythonSample>::insert))
+    .def("insert", [](ListTrial<PythonSample>& out,
+         ListTrial<PythonSample> in) {
+       out.insert(std::make_move_iterator(in.begin()),
+         std::make_move_iterator(in.end()));
+     })
+    .def("__getitem__", &ListTrial<PythonSample>::operator [])
+    .def("__iter__", [](const ListTrial<PythonSample>& t) {
+       return make_iterator(t.begin(), t.end());
+     })
+    .def("__len__", &ListTrial<PythonSample>::size);
 }
