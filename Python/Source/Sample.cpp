ae84b19c105d96d08d01312389925ee88a9caaab
andrei-sa <andrei@spiretrading.com>
2019-03-25 18:42:18 -0400

    Re-format namespaces in Sample.cpp

--- Python/Source/Sample.cpp
+++ Python/Source/Sample.cpp
@@ -1,12 +1,11 @@
 #include "Sample.hpp"
 
 using namespace pybind11;
+using namespace Rover;
 
-namespace Rover {
-  void export_sample(module& module) {
-    class_<PythonSample>(module, "Sample")
-      .def(init<>())
-      .def_readwrite("result", &PythonSample::m_result)
-      .def_readwrite("arguments", &PythonSample::m_arguments);
-  }
+void Rover::export_sample(module& module) {
+  class_<PythonSample>(module, "Sample")
+    .def(init<>())
+    .def_readwrite("result", &PythonSample::m_result)
+    .def_readwrite("arguments", &PythonSample::m_arguments);
 }
