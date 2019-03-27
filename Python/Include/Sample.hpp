80ca9257600163270664c9608ff5c1a24cfdee78
andrei-sa <andrei@spiretrading.com>
2019-03-27 11:15:21 -0400

    Add PythonSample to generic sample conversion

--- Python/Include/Sample.hpp
+++ Python/Include/Sample.hpp
@@ -1,6 +1,8 @@
 #ifndef ROVER_PYTHON_SAMPLE_HPP
 #define ROVER_PYTHON_SAMPLE_HPP
 #include <string_view>
+#include <tuple>
+#include <utility>
 #include <pybind11/pybind11.h>
 
 namespace Rover {
@@ -16,6 +18,23 @@ namespace Rover {
     //! The arguments passed to the function.
     Parameters m_arguments;
   };
+
+namespace Details {
+  template<typename T, std::size_t... I>
+  T cast_arguments(const pybind11::tuple& t, std::index_sequence<I...>) {
+    return std::make_tuple(t[I].template cast<std::tuple_element_t<I, T>>()...);
+  }
+
+
+  template<typename S>
+  S sample_cast(const PythonSample& s) {
+    S result;
+    result.m_result = s.m_result.template cast<typename S::Result>();
+    result.m_arguments = cast_arguments<typename S::Parameters>(s.m_arguments,
+      std::make_index_sequence<std::tuple_size_v<typename S::Parameters>>());
+    return result;
+  }
+}
 
   //! Exports a Sample for Python objects.
   /*!
@@ -38,6 +57,10 @@ namespace Rover {
     auto name = std::string("Sample").append(suffix);
     pybind11::class_<Sample<R, P...>>(module, name.c_str())
       .def(pybind11::init<>())
+      .def(pybind11::init(
+         [](PythonSample s) {
+           return Details::sample_cast<Sample<R, P...>>(s);
+         }))
       .def_readwrite("result", &Sample<R, P...>::m_result)
       .def_readwrite("arguments", &Sample<R, P...>::m_arguments);
     pybind11::implicitly_convertible<Sample<R, P...>, PythonSample>();
