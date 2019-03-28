a4d5fc222a6ad896e0f2912fca6e66c19d065194
andrei-sa <andrei@spiretrading.com>
2019-03-28 17:23:27 -0400

    Add tests for convertible types and custom CompType

--- Tests/Source/LinearRegressionModelTester.cpp
+++ Tests/Source/LinearRegressionModelTester.cpp
@@ -1,9 +1,24 @@
+#include <type_traits>
 #include <catch.hpp>
 #include "Rover/LinearRegressionModel.hpp"
 #include "Rover/ListTrial.hpp"
 #include "Rover/Sample.hpp"
 
 using namespace Rover;
+
+namespace {
+  template<typename T>
+  struct Conv {
+    Conv(T val)
+      : m_val(std::move(val)) {}
+
+    operator T() const {
+      return m_val;
+    }
+
+    T m_val;
+  };
+}
 
 TEST_CASE("test_simple_linear_regression", "[LinearRegressionModel]") {
   SECTION("TwoPoints.") {
@@ -36,6 +51,36 @@ TEST_CASE("test_simple_linear_regression", "[LinearRegressionModel]") {
     REQUIRE(model(2.) == Approx(4.));
     REQUIRE(model(4.) == Approx(4.));
     REQUIRE(model(10.) == Approx(4.));
+  }
+  SECTION("ConvertibleSameType.") {
+    auto trial = ListTrial<Sample<Conv<double>, Conv<double>>>();
+    trial.insert({ Conv<double>(4.), { Conv<double>(1.) } });
+    trial.insert({ Conv<double>(2.), { Conv<double>(0.) } });
+    trial.insert({ Conv<double>(8.), { Conv<double>(3.) } });
+    auto model = LinearRegressionModel(trial);
+    REQUIRE(static_cast<double>(model(Conv<double>(-10.))) == Approx(-18.));
+    REQUIRE(static_cast<double>(model(Conv<double>(0.5))) == Approx(3.));
+    REQUIRE(static_cast<double>(model(Conv<double>(10.))) == Approx(22.));
+  }
+  SECTION("ConvertibleMixedTypes.") {
+    auto trial = ListTrial<Sample<Conv<float>, Conv<double>>>();
+    trial.insert({ Conv<float>(4.f), { Conv<double>(1.) } });
+    trial.insert({ Conv<float>(2.f), { Conv<double>(0.) } });
+    trial.insert({ Conv<float>(8.f), { Conv<double>(3.) } });
+    auto model = LinearRegressionModel(trial);
+    REQUIRE(static_cast<float>(model(Conv<double>(-10.))) == Approx(-18.f));
+    REQUIRE(static_cast<float>(model(Conv<double>(0.5))) == Approx(3.f));
+    REQUIRE(static_cast<float>(model(Conv<double>(10.))) == Approx(22.f));
+  }
+  SECTION("NarrowCompType.") {
+    auto trial = ListTrial<Sample<double, double>>();
+    trial.insert({ 4., { 1. } });
+    trial.insert({ 2., { 0. } });
+    auto model = LinearRegressionModel<std::decay_t<decltype(trial)>, float>(
+      trial);
+    REQUIRE(model(-10.) == Approx(-18.));
+    REQUIRE(model(0.5) == Approx(3.));
+    REQUIRE(model(10.) == Approx(22.));
   }
 }
 
@@ -76,5 +121,49 @@ TEST_CASE("test_multivariable_linear_regression", "[LinearRegressionModel]") {
     REQUIRE(model(1., 1.) == Approx(-2.9073));
     REQUIRE(model(10., 10.) == Approx(28.7981));
     REQUIRE(model(-10., -10.) == Approx(-41.6584));
+  }
+  SECTION("ConvertibleSameType.") {
+    auto trial = ListTrial<Sample<Conv<double>, Conv<double>, Conv<double>>>();
+    trial.insert({ Conv<double>(2.), { Conv<double>(1.), Conv<double>(1.) } });
+    trial.insert({ Conv<double>(8.), { Conv<double>(2.), Conv<double>(4.) } });
+    trial.insert({ Conv<double>(16.), { Conv<double>(4.),
+      Conv<double>(4.) } });
+    auto model = LinearRegressionModel(trial);
+    REQUIRE(static_cast<double>(model(Conv<double>(0.), Conv<double>(0.))) ==
+      Approx(-8. / 3));
+    REQUIRE(static_cast<double>(model(Conv<double>(1.), Conv<double>(0.))) ==
+      Approx(4. / 3));
+    REQUIRE(static_cast<double>(model(Conv<double>(0.), Conv<double>(1.))) ==
+      Approx(-2.));
+    REQUIRE(static_cast<double>(model(Conv<double>(1.), Conv<double>(1.))) ==
+      Approx(2.));
+  }
+  SECTION("ConvertibleMixedTypes.") {
+    auto trial = ListTrial<Sample<Conv<double>, Conv<float>, Conv<double>>>();
+    trial.insert({ Conv<double>(2.), { Conv<float>(1.f), Conv<double>(1.) } });
+    trial.insert({ Conv<double>(8.), { Conv<float>(2.f), Conv<double>(4.) } });
+    trial.insert({ Conv<double>(16.), { Conv<float>(4.f),
+      Conv<double>(4.) } });
+    auto model = LinearRegressionModel(trial);
+    REQUIRE(static_cast<double>(model(Conv<float>(0.f), Conv<double>(0.))) ==
+      Approx(-8. / 3));
+    REQUIRE(static_cast<double>(model(Conv<float>(1.f), Conv<double>(0.))) ==
+      Approx(4. / 3));
+    REQUIRE(static_cast<double>(model(Conv<float>(0.f), Conv<double>(1.))) ==
+      Approx(-2.));
+    REQUIRE(static_cast<double>(model(Conv<float>(1.f), Conv<double>(1.))) ==
+      Approx(2.));
+  }
+  SECTION("NarrowCompType.") {
+    auto trial = ListTrial<Sample<double, double, double>>();
+    trial.insert({ 2., { 1., 1. } });
+    trial.insert({ 8., { 2., 4. } });
+    trial.insert({ 16., { 4., 4. } });
+    auto model = LinearRegressionModel<std::decay_t<decltype(trial)>, float>(
+      trial);
+    REQUIRE(model(0., 0.) == Approx(-8. / 3));
+    REQUIRE(model(1., 0.) == Approx(4. / 3));
+    REQUIRE(model(0., 1.) == Approx(-2.));
+    REQUIRE(model(1., 1.) == Approx(2.));
   }
 }
