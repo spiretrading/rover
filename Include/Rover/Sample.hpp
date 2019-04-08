d002ca6727c81396e0cca332ba44bae30b824229
andrei-sa <andrei@spiretrading.com>
2019-04-08 18:43:36 -0400

    Add visit_arguments for const refs

--- Include/Rover/Sample.hpp
+++ Include/Rover/Sample.hpp
@@ -38,6 +38,19 @@ namespace Rover {
     }, tuple);
   }
 
+  //! Applies a function to every element of a tuple.
+  /*!
+    \param func Function taking a tuple element and its index as arguments.
+    \param tuple Tuple of the arguments.
+  */
+  template<typename Func, typename... A>
+  void visit_arguments(Func&& func, const std::tuple<A...>& tuple) {
+    std::apply([&](auto&&... args) {
+      auto i = std::size_t(0);
+      (func(args, i++), ...);
+    }, tuple);
+  }
+
   //! Returns the number of elements in a tuple.
   template<typename... A>
   constexpr std::size_t arguments_size(const std::tuple<A...>& tuple) {
