4b721055089c7e62979008cb3d9ef33557e16d94
andrei-sa <andrei@spiretrading.com>
2019-04-09 11:12:01 -0400

    Add documentation for TrialReader

--- Include/Rover/TrialReader.hpp
+++ Include/Rover/TrialReader.hpp
@@ -5,28 +5,46 @@
 
 namespace Rover {
 
+  //! Interface to pass adapted Trial Samples to typed Algorithms.
+  /*!
+    \tparam T CompType used by the algorithm.
+  */
   template<typename T>
   class TrialReader {
     public:
 
+      //! CompType used by the algorithm.
       using Type = T;
 
+      //! Adapted sample.
       struct Sample {
 
+        //! The type of the result.
         using Result = Type;
 
+        //! The type of the arguments.
         using Arguments = std::vector<Type>;
 
+        //! The result of the trial's function.
         Result m_result;
 
+        //! The arguments passed to the trial's function.
         Arguments m_arguments;
       };
 
+      //! Creates a TrialReader.
+      /*!
+        \param get The function taking an index i and returning the ith adapted
+                   sample.
+        \param size The total number of samples.
+      */
       template<typename GetFwd>
       TrialReader(GetFwd&& get, std::size_t size);
 
+      //! Returns ith adapted sample.
       Sample operator [](std::size_t i) const;
 
+      //! Returns the number of the samples.
       std::size_t size() const;
 
     private:
