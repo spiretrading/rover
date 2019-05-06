20bd726d80305aff0b83b6e2bbf3fabcc7126f54
andrei-sa <andrei@spiretrading.com>
2019-05-06 13:52:24 -0400

    Fix namespace formatting

--- Python/Source/LinearRegressionModel.cpp
+++ Python/Source/LinearRegressionModel.cpp
@@ -1,8 +1,5 @@
 #include "LinearRegressionModel.hpp"
 
-namespace Rover {
-
-  void export_linear_regression_model(pybind11::module& module) {
-    export_linear_regression_model<PythonSample, double>(module, "");
-  }
+void Rover::export_linear_regression_model(pybind11::module& module) {
+  export_linear_regression_model<PythonSample, double>(module, "");
 }
