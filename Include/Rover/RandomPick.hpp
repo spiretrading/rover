349734b9a540cad337209f30473f74a0a3f4cfbd
andrei-sa <andrei@spiretrading.com>
2019-05-16 15:12:06 -0400

    Add RandomPick generator

---
+++ Include/Rover/RandomPick.hpp
@@ -0,0 +1,70 @@
+#ifndef ROVER_RANDOM_PICK_HPP
+#define ROVER_RANDOM_PICK_HPP
+#include "Rover/Evaluator.hpp"
+#include "Rover/Pick.hpp"
+#include "Rover/Range.hpp"
+
+namespace Rover {
+
+  //! An argument generator that evaluates to one of the arguments with an
+  //! equal likelyhood.
+  /*!
+    \tparam G The types of the generators.
+  */
+  template<typename... G>
+  class RandomPick {
+    private:
+      struct Choice {
+        explicit Choice(std::size_t num_generators);
+        std::size_t operator ()();
+
+        Range<std::size_t, std::size_t> m_range;
+      };
+      using PickType = Pick<Choice, G...>;
+
+    public:
+
+      //! The type of generated values.
+      using Type = typename PickType::Type;
+
+      //! Constructs RandomPick.
+      /*!
+        \param generators One or more generators evaluating to the same type.
+      */
+      template<typename... GeneratorsFwd>
+      explicit RandomPick(GeneratorsFwd&&... generators);
+
+      //! Evaluates the generator.
+      Type generate(Evaluator& evaluator);
+
+    private:
+      PickType m_pick;
+  };
+
+  template<typename... GeneratorsFwd>
+  RandomPick(GeneratorsFwd&&...) -> RandomPick<std::decay_t<GeneratorsFwd>...>;
+
+  template<typename... G>
+  RandomPick<G...>::Choice::Choice(std::size_t num_generators)
+    : m_range(0, num_generators - 1) {}
+
+  template<typename... G>
+  std::size_t RandomPick<G...>::Choice::operator ()() {
+    auto evaluator = Evaluator();
+    return m_range.generate(evaluator);
+  }
+
+  template<typename... G>
+  template<typename... GeneratorsFwd>
+  RandomPick<G...>::RandomPick(GeneratorsFwd&&... generators)
+    : m_pick(Choice(sizeof...(generators)), std::forward<GeneratorsFwd>(
+        generators)...) {}
+
+  template<typename... G>
+  typename RandomPick<G...>::Type RandomPick<G...>::generate(Evaluator&
+      evaluator) {
+    return m_pick.generate(evaluator);
+  }
+}
+
+#endif
---
+++ Tests/Source/RandomPickTester.cpp
@@ -0,0 +1,32 @@
+#include <catch.hpp>
+#include "Rover/Constant.hpp"
+#include "Rover/Generator.hpp"
+#include "Rover/RandomPick.hpp"
+
+using namespace Rover;
+
+TEST_CASE("test_random_pick_deterministic", "[RandomPick]") {
+  SECTION("Single generator.") {
+    auto constant = Constant(5);
+    auto random_pick = RandomPick(constant);
+    REQUIRE(generate(random_pick) == 5);
+  }
+  SECTION("Multiple generators same value.") {
+    auto c1 = Constant(5);
+    auto c2 = Constant(5);
+    auto c3 = Constant(5);
+    auto random_pick = RandomPick(c1, c2, c3);
+    REQUIRE(generate(random_pick) == 5);
+  }
+}
+
+TEST_CASE("test_random_pick_arbitrary", "[RandomPick]") {
+  auto c1 = Constant(3);
+  auto c2 = Constant(5);
+  auto c3 = Constant(7);
+  auto random_pick = RandomPick(c1, c2, c3);
+  for(int i = 0; i < 100; ++i) {
+    auto value = generate(random_pick);
+    REQUIRE((value == 3 || value == 5 || value == 7));
+  }
+}
