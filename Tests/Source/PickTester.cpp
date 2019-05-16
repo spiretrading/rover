d6ad99db969c827c891d9f978ffd19b7a158d5e8
andrei-sa <andrei@spiretrading.com>
2019-05-16 18:10:40 -0400

    Simplify choice generators in PickTester

--- Tests/Source/PickTester.cpp
+++ Tests/Source/PickTester.cpp
@@ -2,62 +2,35 @@
 #include "Rover/Constant.hpp"
 #include "Rover/Evaluator.hpp"
 #include "Rover/Generator.hpp"
+#include "Rover/Lift.hpp"
 #include "Rover/Pick.hpp"
 
 using namespace Rover;
 
 TEST_CASE("test_pick_fixed_index", "[Pick]") {
   SECTION("Single generator.") {
-    struct Choice {
-      public:
-        using Type = std::size_t;
-
-        Type generate(Evaluator& evaluator) {
-          return 0;
-        }
-    };
-    auto pick = Pick(Choice(), Constant(5));
+    auto pick = Pick(Constant<std::size_t>(0), Constant(5));
     REQUIRE(generate(pick) == 5);
   }
   SECTION("Multiple generators.") {
-    struct Choice {
-      public:
-        using Type = std::size_t;
-
-        Type generate(Evaluator& evaluator) {
-          return 1;
-        }
-    };
     auto c1 = Constant(3);
     auto c2 = Constant(5);
     auto c3 = Constant(7);
-    auto pick = Pick(Choice(), c1, c2, c3);
+    auto pick = Pick(Constant<std::size_t>(1), c1, c2, c3);
     REQUIRE(generate(pick) == 5);
   }
 }
 
 TEST_CASE("test_pick_variable_index", "[Pick]") {
-  struct Choice {
-    public:
-      using Type = std::size_t;
-
-      Choice()
-        : m_counter(0) {}
-
-      Type generate(Evaluator& evaluator) {
-        auto index = m_counter % 3;
-        ++m_counter;
-        return index;
-      }
-
-    private:
-      std::size_t m_counter;
-  };
   auto c1 = Constant(3);
   auto c2 = Constant(5);
   auto c3 = Constant(7);
   auto i = std::size_t(0);
-  auto pick = Pick(Choice(), c1, c2, c3);
+  auto pick = Pick(Lift([&] {
+    auto index = i % 3;
+    ++i;
+    return index;
+  }), c1, c2, c3);
   REQUIRE(generate(pick) == 3);
   REQUIRE(generate(pick) == 5);
   REQUIRE(generate(pick) == 7);
